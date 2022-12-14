1e2d6d5d5f299bc3d753e091fb8d488e2fef6f6c
Mauricio <mauricio.sjn@gmail.com>
2022-12-14 17:19:56 -0300

    Updates

---
+++ precache-manifest.5413afcbbb05a3031980c408925c0518.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12affc4f5431dd7bd990",
+    "url": "/Web-Curriculum-Mauricio-Carrion/static/css/main.ffd7b2bf.chunk.css"
+  },
+  {
+    "revision": "12affc4f5431dd7bd990",
+    "url": "/Web-Curriculum-Mauricio-Carrion/static/js/main.12affc4f.chunk.js"
+  },
+  {
+    "revision": "08818c38f200658407da",
+    "url": "/Web-Curriculum-Mauricio-Carrion/static/js/1.08818c38.chunk.js"
+  },
+  {
+    "revision": "f78518e6c429d7893769",
+    "url": "/Web-Curriculum-Mauricio-Carrion/static/js/runtime~main.f78518e6.js"
+  },
+  {
+    "revision": "1e93619b1d7a684621718829488d3df3",
+    "url": "/Web-Curriculum-Mauricio-Carrion/static/media/20200401_130928.1e93619b.jpg"
+  },
+  {
+    "revision": "890d9ed927bb7f0c6b9eed04f9c329e5",
+    "url": "/Web-Curriculum-Mauricio-Carrion/static/media/Quicksand-Medium.890d9ed9.ttf"
+  },
+  {
+    "revision": "b16d40c28fd77e9a80f0c8762bf6c0f8",
+    "url": "/Web-Curriculum-Mauricio-Carrion/index.html"
+  }
+];
